0eddf414ce7bd025e9545792fd917aa90434728c
sumanth <sumanth.m@techsophy.com>
2019-12-12 16:28:39 +0530

    initial setup

---
+++ src/main/java/com/sumanth/firstSpringProject/FirstSpringProjectApplication.java
@@ -0,0 +1,14 @@
+package com.sumanth.firstSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringProjectApplication.class, args);
+		System.out.println("----------------Spring Tutorial step one -----------------");
+	}
+
+}
